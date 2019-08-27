57872541ce136ad9b47f43e1713c021c990640cf
Jamie Warburton <jamie@hexdigital.com>
2019-08-27 16:04:44 +0100

    [typescript] Add typescript, eslint, and prettier. Allow eslint to lint typescript, and run prettier as an eslint rule

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true, // Don't search up parent folders for more eslintrc files
+    plugins: ['@typescript-eslint'],
+    parserOptions:  {
+        parser:  '@typescript-eslint/parser',
+        ecmaVersion: 2018,
+        sourceType: 'module',  // Allows for the use of imports
+        ecmaFeatures: {
+            legacyDecorators: true,
+        },
+    },
+    extends:  [
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
+    rules: {
+        '@typescript-eslint/no-unused-vars': 'error',
+    },
+};
