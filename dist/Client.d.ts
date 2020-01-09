6188cece1ec323d38bee9e49e8768d0cb1aab075
Bella Taylor <bellataylorlondon@gmail.com>
2020-01-09 17:11:01 +0000

    Add dist files

--- dist/Client.d.ts
+++ dist/Client.d.ts
@@ -2,9 +2,11 @@ import ApiOptions from './Interfaces/ApiOptions';
 import HttpClient from './Interfaces/HttpClient';
 import Auth from './Resources/Auth/auth';
 import Search from './Resources/Search/Search';
+import ReturnOrders from './Resources/Returns/returnOrders';
 export default class Client {
     search: Search;
     auth: Auth;
+    returnOrders: ReturnOrders;
     protected apiOptions: ApiOptions;
     protected httpClient: HttpClient;
     constructor(httpClient: HttpClient);
--- dist/Resources/index.js
+++ dist/Resources/index.js
@@ -9,11 +9,14 @@ var _auth = _interopRequireDefault(require("./Auth/auth"));
 
 var _Search = _interopRequireDefault(require("./Search/Search"));
 
+var _returnOrders = _interopRequireDefault(require("./Returns/returnOrders"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var _default = {
   Auth: _auth["default"],
-  Search: _Search["default"]
+  Search: _Search["default"],
+  ReturnOrders: _returnOrders["default"]
 };
 exports["default"] = _default;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9SZXNvdXJjZXMvaW5kZXgudHMiXSwibmFtZXMiOlsiQXV0aCIsIlNlYXJjaCJdLCJtYXBwaW5ncyI6Ijs7Ozs7OztBQUFBOztBQUNBOzs7O2VBRWU7QUFDWEEsRUFBQUEsSUFBSSxFQUFKQSxnQkFEVztBQUVYQyxFQUFBQSxNQUFNLEVBQU5BO0FBRlcsQyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBBdXRoIGZyb20gJy4vQXV0aC9hdXRoJztcbmltcG9ydCBTZWFyY2ggZnJvbSAnLi9TZWFyY2gvU2VhcmNoJztcblxuZXhwb3J0IGRlZmF1bHQge1xuICAgIEF1dGgsXG4gICAgU2VhcmNoLFxufTtcbiJdfQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9SZXNvdXJjZXMvaW5kZXgudHMiXSwibmFtZXMiOlsiQXV0aCIsIlNlYXJjaCIsIlJldHVybk9yZGVycyJdLCJtYXBwaW5ncyI6Ijs7Ozs7OztBQUFBOztBQUNBOztBQUNBOzs7O2VBRWU7QUFDWEEsRUFBQUEsSUFBSSxFQUFKQSxnQkFEVztBQUVYQyxFQUFBQSxNQUFNLEVBQU5BLGtCQUZXO0FBR1hDLEVBQUFBLFlBQVksRUFBWkE7QUFIVyxDIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IEF1dGggZnJvbSAnLi9BdXRoL2F1dGgnO1xuaW1wb3J0IFNlYXJjaCBmcm9tICcuL1NlYXJjaC9TZWFyY2gnO1xuaW1wb3J0IFJldHVybk9yZGVycyBmcm9tICcuL1JldHVybnMvcmV0dXJuT3JkZXJzJztcblxuZXhwb3J0IGRlZmF1bHQge1xuICAgIEF1dGgsXG4gICAgU2VhcmNoLFxuICAgIFJldHVybk9yZGVycyxcbn07XG4iXX0=
