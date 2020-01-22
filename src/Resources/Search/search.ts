53791986280b027a4dbc9decc132dcddc9df4c7f
Andrew Mc Cormack <webmaster@ddmseo.com>
2020-01-22 15:25:20 +0000

    update search endpoint to pass term

--- src/Resources/Search/search.ts
+++ src/Resources/Search/search.ts
@@ -2,12 +2,14 @@ import AbstractResource from '../AbstractResource';
 
 export default class Search extends AbstractResource {
     initialise(): void {
-        this.resourcePath = 'search';
+        this.resourcePath = 'wacc/search';
     }
 
-    public all: Function = this.createMethodFromPartialSpec({
-        method: 'GET',
-        // methodType: 'list', // @todo Add this once auto-pagination is complete
-        path: '/',
-    });
+    public all(term: string): Function {
+        return this.createMethodFromPartialSpec({
+            method: 'GET',
+            // methodType: 'list', // @todo Add this once auto-pagination is complete
+            path: `?search=${term}`,
+        });
+    }
 }
