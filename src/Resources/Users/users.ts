fb0a257ab78ea831733bbedf425c8a91906a06e7
jrods <jessica@whirli.com>
2020-01-30 12:02:50 +0000

    fix lint issues

--- src/Resources/Users/users.ts
+++ src/Resources/Users/users.ts
@@ -8,25 +8,25 @@ export default class Users extends AbstractResource {
     public all: Function = this.createMethodFromPartialSpec({
         method: 'GET',
         path: '/',
-    })
+    });
 
     public get: Function = this.createMethodFromPartialSpec({
         method: 'GET',
         path: '/{id}',
-    })
+    });
 
     public create: Function = this.createMethodFromPartialSpec({
         method: 'POST',
         path: '/',
-    })
+    });
 
     public update: Function = this.createMethodFromPartialSpec({
         method: 'PUT',
         path: '/{id}',
-    })
+    });
 
     public delete: Function = this.createMethodFromPartialSpec({
         method: 'DELETE',
         path: '/{id}',
-    })
+    });
 }
