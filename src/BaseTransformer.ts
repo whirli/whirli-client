bcbd6edb7c74f5bc4c4ec0bdc64862d6a889fc98
Bella Taylor <bellataylorlondon@gmail.com>
2020-01-31 18:15:49 +0000

    Add a BaseTransformer

---
+++ src/BaseTransformer.ts
@@ -0,0 +1,46 @@
+import { transform, transformCollection } from './helpers/transformers';
+
+class BaseTransformer {
+    /**
+     * Perform the data mapping - this should be overridden by each specific Transformer
+     *
+     * @param data
+     * @param args
+     */
+    mapData(data: object, ...args: Array<any>) {
+        return {
+            data,
+            ...args,
+        };
+    }
+
+    /**
+     * Transform an attribute if in includes, and the data exists
+     *
+     * @param data
+     * @param includeName
+     * @param transformer
+     */
+    item(data: Record<string, any>, includeName: string, transformer: BaseTransformer) {
+        if (data[includeName]) {
+            return transform(data[includeName], transformer);
+        }
+        return null;
+    }
+
+    /**
+     * Transform a collection of attributes if in includes, and the data exists
+     *
+     * @param data
+     * @param includeName
+     * @param transformer
+     */
+    collection(data: Record<string, any>, includeName: string, transformer: BaseTransformer) {
+        if (data[includeName]) {
+            return transformCollection(data[includeName], transformer);
+        }
+        return null;
+    }
+}
+
+export default BaseTransformer;
