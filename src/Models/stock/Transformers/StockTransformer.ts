51a059935b35bac0b541058396366a014e61ed96
Bella Taylor <bellataylorlondon@gmail.com>
2020-02-05 20:27:56 +0000

    Add stock models transformers

---
+++ src/Models/stock/Transformers/StockTransformer.ts
@@ -0,0 +1,40 @@
+import Stock from '../Stock';
+import StockInterface from '../../../Interfaces/stock/Stock';
+import ProductVariantInterface from '../../../Interfaces/product/ProductVariant';
+import UserInterface from '../../../Interfaces/user/User';
+import ReturnOrderLineInterface from '../../../Interfaces/return/ReturnOrderLine';
+import UserTransformer from '../../user/Transformers/UserTransformer';
+import ProductVariantTransformer from '../../product/Transformers/ProductVariantTransformer';
+import ReturnOrderLineTransformer from '../../return/Transformers/ReturnOrderLineTransformer';
+import BaseTransformer from '../../../BaseTransformer';
+
+export default class StockTransformer extends BaseTransformer {
+    /**
+     * Map an stock response
+     *
+     * @param stock
+     */
+    mapData(stock: StockInterface): Stock {
+        return new Stock({
+            id: stock.id,
+            createdAt: stock.createdAt,
+            updatedAt: stock.updatedAt,
+            statusId: stock.statusId,
+            // belongs to
+            productVariant: this.includeProductVariant(stock),
+            user: this.includeUser(stock),
+            // has many
+            returnOrderLines: this.includeReturnOrderLines(stock.returnOrderLines || []),
+        });
+    }
+
+    includeProductVariant(stock: StockInterface): ProductVariantInterface | null {
+        return this.item(stock, 'productVariant', new ProductVariantTransformer());
+    }
+    includeUser(stock: StockInterface): UserInterface | null {
+        return this.item(stock, 'user', new UserTransformer());
+    }
+    includeReturnOrderLines(stock: StockInterface[]): ReturnOrderLineInterface[] {
+        return this.collection(stock, 'returnOrderLines', new ReturnOrderLineTransformer());
+    }
+}
