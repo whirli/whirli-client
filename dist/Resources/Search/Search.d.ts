258c69b9291d1a67d1ec5848a356d8447f3e3491
Bella Taylor <bellataylorlondon@gmail.com>
2020-02-06 16:42:40 +0000

    Undo changes in partial spec

--- dist/Resources/Search/Search.d.ts
+++ dist/Resources/Search/Search.d.ts
@@ -1,5 +1,5 @@
 import AbstractResource from '../AbstractResource';
 export default class Search extends AbstractResource {
     initialise(): void;
-    all(): Function;
+    all(term: string): Function;
 }
--- src/Interfaces/PartialSpec.ts
+++ src/Interfaces/PartialSpec.ts
@@ -1,5 +1,5 @@
 export default interface PartialSpec {
     method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'OPTIONS' | 'DELETE';
     methodType?: string;
-    path?: string | {};
+    path?: string;
 }
