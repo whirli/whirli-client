aa4111543fee8122197a0a27328386e20f4ef809
Bella Taylor <bellataylorlondon@gmail.com>
2020-02-06 18:45:28 +0000

    Rebuild dist files

--- dist/Interfaces/ApiRequestBody.d.ts
+++
@@ -1,3 +0,0 @@
-export default interface ApiRequestBody {
-    [key: string]: any;
-}
--- dist/Interfaces/LoginBody.d.ts
+++
@@ -1,5 +0,0 @@
-import ApiRequestBody from './ApiRequestBody';
-export default interface LoginBody extends ApiRequestBody {
-    email: string;
-    password: string;
-}
