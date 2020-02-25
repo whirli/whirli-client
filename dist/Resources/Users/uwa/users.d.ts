028cd808fbe2a71a660c831673801ca50f08ddbe
Jamie Warburton <jamie@hexdigital.com>
2020-02-25 14:55:15 +0000

    [separate-wacc-and-uwa] Remove dist directory before rebuilding in lint-staged

--- dist/Resources/Users/uwa/users.d.ts
+++
@@ -1,8 +0,0 @@
-import AbstractResource from '../../AbstractResource';
-export default class Users extends AbstractResource {
-    initialise(): void;
-    get: Function;
-    create: Function;
-    update: Function;
-    delete: Function;
-}
--- dist/Resources/Users/wacc/users.d.ts
+++
@@ -1,9 +0,0 @@
-import AbstractResource from '../../AbstractResource';
-export default class Users extends AbstractResource {
-    initialise(): void;
-    all: Function;
-    get: Function;
-    create: Function;
-    update: Function;
-    delete: Function;
-}
