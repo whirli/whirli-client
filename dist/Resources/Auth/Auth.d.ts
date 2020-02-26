a7321ab1507c91b01622704bd681867eef42b68d
Jamie Warburton <jamie@hexdigital.com>
2020-02-26 15:50:54 +0000

    [fix-auth] Correct the method names for auth password calls

--- dist/Resources/Auth/Auth.d.ts
+++ dist/Resources/Auth/Auth.d.ts
@@ -1,6 +1,6 @@
 import AbstractResource from '../AbstractResource';
 export default class Auth extends AbstractResource {
     initialise(): void;
-    login: Function;
-    register: Function;
+    forgotPassword: Function;
+    resetPassword: Function;
 }
