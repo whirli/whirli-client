f8a82386f4fd185f32bc0310e4d0a73777e2e60d
Bella Taylor <bellataylorlondon@gmail.com>
2020-05-05 14:34:13 +0100

    Add to wacc resource index

--- dist/Resources/Wacc/index.d.ts
+++ dist/Resources/Wacc/index.d.ts
@@ -4,11 +4,13 @@ import WACCReturnOrders from './ReturnOrders/ReturnOrders';
 import WACCSearch from './Search/Search';
 import WACCUsers from './Users/Users';
 import WACCGifts from './Gifts/Gifts';
+import WACCProducts from './Products/Products';
 export interface WaccResources {
     orders: WACCOrders;
     returnOrders: WACCReturnOrders;
     search: WACCSearch;
     users: WACCUsers;
     gifts: WACCGifts;
+    products: WACCProducts;
 }
 export declare function loadWaccResources(client: Client): WaccResources;
