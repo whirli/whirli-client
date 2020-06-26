10ae9052df86d561f2e89a39e53c0899deace0e7
johnnewman <newmanj903@gmail.com>
2020-06-26 08:25:02 +0100

    fix checkout endpoint

---
+++ dist/Resources/Users/Checkout.d.ts
@@ -0,0 +1,6 @@
+import AbstractResource from '../AbstractResource';
+import { HttpClientConfig, HttpClientResponse } from "../../Interfaces/HttpClient";
+export default class Checkout extends AbstractResource {
+    initialise(): void;
+    checkout: (...args: HttpClientConfig) => HttpClientResponse;
+}
