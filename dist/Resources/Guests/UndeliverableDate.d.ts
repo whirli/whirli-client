f24f926005e02f0a9e123b4e8457637999f10658
johnnewman <newmanj903@gmail.com>
2020-06-30 15:54:35 +0100

    remove uneeded files

--- dist/Resources/Guests/UndeliverableDate.d.ts
+++
@@ -1,6 +0,0 @@
-import AbstractResource from '../AbstractResource';
-import { HttpClientConfig, HttpClientResponse } from '../../Interfaces/HttpClient';
-export default class UndeliverableDate extends AbstractResource {
-    initialise(): void;
-    get: (...args: HttpClientConfig) => HttpClientResponse;
-}
