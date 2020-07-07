46eb5aa53caac1158a5caf29e9c32ec523bc603a
Giuseppe Zeraschi <giuseppezeraschi@gmail.com>
2020-07-07 13:45:17 +0100

    :bug: Allow referredUsers to be empty array

--- dist/Interfaces/referral/Referral.d.ts
+++ dist/Interfaces/referral/Referral.d.ts
@@ -2,6 +2,6 @@ import ReferredUser from './ReferredUser';
 export default interface Referral {
     code: string;
     totalEarned: number;
-    referredUsers: Array<ReferredUser>;
+    referredUsers: Array<ReferredUser> | [];
     userBalance: number;
 }
