f83990e7a90557f4754358ef05c5abb9b5f616c2
jrods <jessica@whirli.com>
2020-09-18 11:31:39 +0100

    add subscription tokens to user interface

--- dist/Interfaces/user/User.d.ts
+++ dist/Interfaces/user/User.d.ts
@@ -55,6 +55,7 @@ export default interface User {
     toyboxTokens?: number;
     additionalTokens?: number;
     availableTokens?: number;
+    subscriptionTokens?: number;
     maxTokens?: number;
     basketQuantity?: number;
     activeBasketTotalTokens?: number;
