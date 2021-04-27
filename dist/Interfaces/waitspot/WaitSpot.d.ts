5cccf033ea9a3b8bc7700a5428c98a75a459c630
jrods <jessica@whirli.com>
2021-04-27 09:40:14 +0100

    [feature/WHIR-1819] add status id to waitspot interface for wacc

--- dist/Interfaces/waitspot/WaitSpot.d.ts
+++ dist/Interfaces/waitspot/WaitSpot.d.ts
@@ -2,6 +2,7 @@ import ProductVariant from '../product/ProductVariant';
 import User from '../user/User';
 export default interface WaitSpot {
     id?: string;
+    statusId?: number;
     createdAt?: Date | string;
     updatedAt?: Date | string;
     completedAt?: Date | string | null;
--- src/Interfaces/waitspot/WaitSpot.ts
+++ src/Interfaces/waitspot/WaitSpot.ts
@@ -3,6 +3,7 @@ import User from '../user/User';
 
 export default interface WaitSpot {
     id?: string;
+    statusId?: number;
     createdAt?: Date | string;
     updatedAt?: Date | string;
     completedAt?: Date | string | null;
