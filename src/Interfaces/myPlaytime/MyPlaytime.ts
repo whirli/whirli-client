99808cbe046de30a5576e7ad62737624a536319e
Liam Hall <liam@whirli.com>
2021-05-17 17:08:30 +0100

    Update interface

--- src/Interfaces/myPlaytime/MyPlaytime.ts
+++ src/Interfaces/myPlaytime/MyPlaytime.ts
@@ -1,6 +1,6 @@
 export default interface MyPlaytime {
-    total_toys_cost: number;
-    total_toys: number;
-    time_with_us: string;
-    average_token_usage: number;
+    totalToysCost: number;
+    totalToys: number;
+    timeWithUs: string;
+    averageTokenUsage: number;
 }
