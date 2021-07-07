a79c87ec19692d3872e67fb3244405d06ec41b4e
jrods <jessica@whirli.com>
2021-07-07 13:04:25 +0100

    [change/WHIR-2220] fix subscription related types

--- src/Interfaces/user/UserSubscriptionAddon.ts
+++ src/Interfaces/user/UserSubscriptionAddon.ts
@@ -4,8 +4,8 @@ export default interface UserSubscriptionAddon {
     id?: string;
     createdAt?: Date | string;
     updatedAt?: Date | string;
-    userSubscriptionId?: number;
-    subscriptionAddonPricingPlanId?: number;
+    userSubscriptionId?: string;
+    subscriptionAddonPricingPlanId?: string;
     startedAt?: Date | string | null;
     endedAt?: Date | string | null;
     subscriptionAddonPricingPlan?: SubscriptionAddonPricingPlan;
