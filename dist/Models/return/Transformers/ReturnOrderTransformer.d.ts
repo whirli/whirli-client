48aad29986f9b4bd0b90755a1d942590c2c735dd
Andrew Mc Cormack <webmaster@ddmseo.com>
2022-02-23 11:01:48 +0000

    [WHIR-3039] build

--- dist/Models/return/Transformers/ReturnOrderTransformer.d.ts
+++ dist/Models/return/Transformers/ReturnOrderTransformer.d.ts
@@ -4,9 +4,11 @@ import ReturnOrderLineInterface from '../../../Interfaces/return/ReturnOrderLine
 import UserInterface from '../../../Interfaces/user/User';
 import BaseTransformer from '../../../BaseTransformer';
 import ShippingAddonInterface from '../../../Interfaces/delivery/ShippingAddon';
+import ShippingManifestInterface from '../../../Interfaces/delivery/ShippingManifestInterface';
 export default class ReturnOrderTransformer extends BaseTransformer {
     mapData(returnOrder: ReturnOrderInterface): ReturnOrder;
     includeUser(returnOrder: ReturnOrderInterface): UserInterface | null;
+    includeShippingManifest(returnOrder: ReturnOrderInterface): ShippingManifestInterface | null;
     includeOrderLines(returnOrder: ReturnOrderInterface): ReturnOrderLineInterface[];
     includeShippingAddons(returnOrder: ReturnOrderInterface): ShippingAddonInterface[];
 }
